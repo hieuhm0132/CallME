54301a3c89bfe9a194e81145ab15990e493d5837
Hoang Minh Hieu <hieucoi@MacBook-Pro-cua-Hoang-2.local>
2021-09-30 11:53:11 +0700

    Add random quote

--- dist/app.js
+++ dist/app.js
@@ -6,25 +6,67 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const firebase_admin_1 = __importDefault(require("firebase-admin"));
 const agora_access_token_1 = __importDefault(require("agora-access-token"));
-const app = express_1.default();
+const mongoose_1 = __importDefault(require("mongoose"));
+const app = (0, express_1.default)();
 app.use(express_1.default.json());
+const dbConnect = "mongodb+srv://admin:admin@cluster0.gfyr4.mongodb.net/tinder?retryWrites=true&w=majority";
+const options = {
+    autoIndex: false,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+    family: 4, // Use IPv4, skip trying IPv6
+};
+mongoose_1.default.connect(dbConnect, options);
+const db = mongoose_1.default.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function () {
+    console.log("connected");
+});
+const quote = new mongoose_1.default.Schema({
+    quote: String,
+    author: String,
+});
 const appId = "68371bfc640d47a091b607b32dd6599f";
 const appCertificate = "f354e072508440c2bf731b600e7f62ea";
 const expirationTimeInSeconds = 3600;
 const serviceAccount = require("../appcall-95336-firebase-adminsdk-gldkk-2bdb0b1707.json");
 firebase_admin_1.default.initializeApp({
     credential: firebase_admin_1.default.credential.cert(serviceAccount),
-    databaseURL: "https://appcall-95336-default-rtdb.firebaseio.com"
+    databaseURL: "https://appcall-95336-default-rtdb.firebaseio.com",
 });
 const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
     res.send("Working!");
 });
+app.get("/quote/random", (req, res) => {
+    const baseJson = {
+        errorCode: undefined,
+        errorMessage: undefined,
+        data: undefined,
+    };
+    db.model("quotes", quote).find({}, (err, listquote) => {
+        if (err) {
+            baseJson.errorCode = 403;
+            baseJson.errorMessage = "403 Forbidden";
+            baseJson.data = [];
+        }
+        else {
+            const randomquote = listquote[Math.floor(Math.random() * listquote.length)];
+            console.log(randomquote);
+            baseJson.errorCode = 200;
+            baseJson.errorMessage = "OK";
+            baseJson.data = randomquote;
+        }
+        res.send(baseJson);
+    });
+});
 app.post("/sendNotification", (req, res) => {
     console.log(req.body);
     const uidCaller = Math.floor(Math.random() * 100000);
     const uidReceiver = Math.floor(Math.random() * 100000);
-    const role = req.body.isPublisher ? agora_access_token_1.default.RtcRole.PUBLISHER : agora_access_token_1.default.RtcRole.SUBSCRIBER;
+    const role = req.body.isPublisher
+        ? agora_access_token_1.default.RtcRole.PUBLISHER
+        : agora_access_token_1.default.RtcRole.SUBSCRIBER;
     const channel = Math.floor(Math.random() * 100000).toString();
     const currentTimestamp = Math.floor(Date.now() / 1000);
     const expirationTimestamp = currentTimestamp + expirationTimeInSeconds;
@@ -35,37 +77,45 @@ app.post("/sendNotification", (req, res) => {
         rtctoken: tokenReceiver,
         uid: uidReceiver,
         appId: appId,
-        callerFCMToken: req.body.callerFCMToken
+        callerFCMToken: req.body.callerFCMToken,
     };
     const message = {
         notification: {
-            title: 'A Call Incoming!',
-            body: req.body.message
+            title: "A Call Incoming!",
+            body: req.body.message,
         },
         token: req.body.receiverFCMToken,
         data: {
-            json: JSON.stringify(data)
-        }
+            json: JSON.stringify(data),
+        },
     };
-    firebase_admin_1.default.messaging().send(message).then(() => {
-        console.log('Sent Call Notification');
+    firebase_admin_1.default
+        .messaging()
+        .send(message)
+        .then(() => {
+        console.log("Sent Call Notification");
         res.send({ uidCaller, appId, channel, tokenCaller });
-    }).catch(err => {
+    })
+        .catch((err) => {
         console.log(err);
     });
 });
 app.post("/rejectCall", (req, res) => {
     const message = {
         notification: {
-            title: 'Call Rejected!',
-            body: req.body.message
+            title: "Call Rejected!",
+            body: req.body.message,
         },
         token: req.body.token,
     };
-    firebase_admin_1.default.messaging().send(message).then(() => {
-        console.log('Sent Reject Notification');
-        res.send('Done');
-    }).catch(err => {
+    firebase_admin_1.default
+        .messaging()
+        .send(message)
+        .then(() => {
+        console.log("Sent Reject Notification");
+        res.send("Done");
+    })
+        .catch((err) => {
         console.log(err);
     });
     // res.send({ uid, appId, channel, token });
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,32 @@
 import express from "express";
-import admin from 'firebase-admin';
+import admin from "firebase-admin";
 import Agora from "agora-access-token";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(express.json());
+
+const dbConnect =
+  "mongodb+srv://admin:admin@cluster0.gfyr4.mongodb.net/tinder?retryWrites=true&w=majority";
+
+const options = {
+  autoIndex: false, // Don't build indexes
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4, // Use IPv4, skip trying IPv6
+};
+
+mongoose.connect(dbConnect, options);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function () {
+  console.log("connected");
+});
+
+const quote = new mongoose.Schema({
+  quote: String,
+  author: String,
+});
 
 const appId = "68371bfc640d47a091b607b32dd6599f";
 const appCertificate = "f354e072508440c2bf731b600e7f62ea";
@@ -13,7 +36,7 @@ const serviceAccount = require("../appcall-95336-firebase-adminsdk-gldkk-2bdb0b1
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://appcall-95336-default-rtdb.firebaseio.com"
+  databaseURL: "https://appcall-95336-default-rtdb.firebaseio.com",
 });
 
 const port = process.env.PORT || 3000;
@@ -22,61 +45,107 @@ app.get("/", (req, res) => {
   res.send("Working!");
 });
 
+app.get("/quote/random", (req, res) => {
+  const baseJson = {
+    errorCode: undefined,
+    errorMessage: undefined,
+    data: undefined,
+  };
+
+  db.model("quotes", quote).find({}, (err, listquote) => {
+    if (err) {
+      baseJson.errorCode = 403;
+      baseJson.errorMessage = "403 Forbidden";
+      baseJson.data = [];
+    } else {
+      const randomquote =
+        listquote[Math.floor(Math.random() * listquote.length)];
+      console.log(randomquote);
+      baseJson.errorCode = 200;
+      baseJson.errorMessage = "OK";
+      baseJson.data = randomquote;
+    }
+    res.send(baseJson);
+  });
+});
+
 app.post("/sendNotification", (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
 
   const uidCaller = Math.floor(Math.random() * 100000);
   const uidReceiver = Math.floor(Math.random() * 100000);
-  const role = req.body.isPublisher ? Agora.RtcRole.PUBLISHER : Agora.RtcRole.SUBSCRIBER;
+  const role = req.body.isPublisher
+    ? Agora.RtcRole.PUBLISHER
+    : Agora.RtcRole.SUBSCRIBER;
   const channel = Math.floor(Math.random() * 100000).toString();
   const currentTimestamp = Math.floor(Date.now() / 1000);
   const expirationTimestamp = currentTimestamp + expirationTimeInSeconds;
-  const tokenCaller = Agora.RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channel, uidCaller, role, expirationTimestamp);
-  const tokenReceiver = Agora.RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channel, uidReceiver, role, expirationTimestamp);
+  const tokenCaller = Agora.RtcTokenBuilder.buildTokenWithUid(
+    appId,
+    appCertificate,
+    channel,
+    uidCaller,
+    role,
+    expirationTimestamp
+  );
+  const tokenReceiver = Agora.RtcTokenBuilder.buildTokenWithUid(
+    appId,
+    appCertificate,
+    channel,
+    uidReceiver,
+    role,
+    expirationTimestamp
+  );
 
   const data = {
     channel: channel,
     rtctoken: tokenReceiver,
     uid: uidReceiver,
     appId: appId,
-    callerFCMToken: req.body.callerFCMToken
-  }
+    callerFCMToken: req.body.callerFCMToken,
+  };
 
   const message = {
     notification: {
-      title: 'A Call Incoming!',
-      body: req.body.message
+      title: "A Call Incoming!",
+      body: req.body.message,
     },
     token: req.body.receiverFCMToken,
     data: {
-      json: JSON.stringify(data)
-    }
-  }
+      json: JSON.stringify(data),
+    },
+  };
 
-  admin.messaging().send(message).then(() => {
-    console.log('Sent Call Notification')
-    res.send({ uidCaller, appId, channel, tokenCaller });
-  }).catch(err => {
-    console.log(err)
-  })
-})
-
-
+  admin
+    .messaging()
+    .send(message)
+    .then(() => {
+      console.log("Sent Call Notification");
+      res.send({ uidCaller, appId, channel, tokenCaller });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.post("/rejectCall", (req, res) => {
   const message = {
     notification: {
-      title: 'Call Rejected!',
-      body: req.body.message
+      title: "Call Rejected!",
+      body: req.body.message,
     },
     token: req.body.token,
-  }
-  admin.messaging().send(message).then(() => {
-    console.log('Sent Reject Notification')
-    res.send('Done')
-  }).catch(err => {
-    console.log(err)
-  })
+  };
+  admin
+    .messaging()
+    .send(message)
+    .then(() => {
+      console.log("Sent Reject Notification");
+      res.send("Done");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   // res.send({ uid, appId, channel, token });
 });
 
